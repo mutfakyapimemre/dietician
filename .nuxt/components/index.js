1e5ccb7183c89824fc5d71757dfd2b29dbe88a41
mutfakyapimemre <emrekilic@mutfakyapim.com>
2021-01-15 11:19:36 +0300

    pcs

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,17 +1,17 @@
-export { default as Dietician_Header } from '../..\\components\\dietician_includes\\Dietician_Header.vue'
-export { default as Dietician_Sidebar } from '../..\\components\\dietician_includes\\Dietician_Sidebar.vue'
+export { default as Admin_Header } from '../..\\components\\admin_includes\\Admin_Header.vue'
+export { default as Admin_Sidebar } from '../..\\components\\admin_includes\\Admin_Sidebar.vue'
 export { default as DieticianSidebar } from '../..\\components\\includes\\DieticianSidebar.vue'
 export { default as Footer } from '../..\\components\\includes\\Footer.vue'
 export { default as Header } from '../..\\components\\includes\\Header.vue'
 export { default as Sidebar } from '../..\\components\\includes\\Sidebar.vue'
-export { default as Admin_Header } from '../..\\components\\admin_includes\\Admin_Header.vue'
-export { default as Admin_Sidebar } from '../..\\components\\admin_includes\\Admin_Sidebar.vue'
+export { default as Dietician_Header } from '../..\\components\\dietician_includes\\Dietician_Header.vue'
+export { default as Dietician_Sidebar } from '../..\\components\\dietician_includes\\Dietician_Sidebar.vue'
 
-export const LazyDietician_Header = import('../..\\components\\dietician_includes\\Dietician_Header.vue' /* webpackChunkName: "components/dietician-header" */).then(c => c.default || c)
-export const LazyDietician_Sidebar = import('../..\\components\\dietician_includes\\Dietician_Sidebar.vue' /* webpackChunkName: "components/dietician-sidebar" */).then(c => c.default || c)
+export const LazyAdmin_Header = import('../..\\components\\admin_includes\\Admin_Header.vue' /* webpackChunkName: "components/admin-header" */).then(c => c.default || c)
+export const LazyAdmin_Sidebar = import('../..\\components\\admin_includes\\Admin_Sidebar.vue' /* webpackChunkName: "components/admin-sidebar" */).then(c => c.default || c)
 export const LazyDieticianSidebar = import('../..\\components\\includes\\DieticianSidebar.vue' /* webpackChunkName: "components/dietician-sidebar" */).then(c => c.default || c)
 export const LazyFooter = import('../..\\components\\includes\\Footer.vue' /* webpackChunkName: "components/footer" */).then(c => c.default || c)
 export const LazyHeader = import('../..\\components\\includes\\Header.vue' /* webpackChunkName: "components/header" */).then(c => c.default || c)
 export const LazySidebar = import('../..\\components\\includes\\Sidebar.vue' /* webpackChunkName: "components/sidebar" */).then(c => c.default || c)
-export const LazyAdmin_Header = import('../..\\components\\admin_includes\\Admin_Header.vue' /* webpackChunkName: "components/admin-header" */).then(c => c.default || c)
-export const LazyAdmin_Sidebar = import('../..\\components\\admin_includes\\Admin_Sidebar.vue' /* webpackChunkName: "components/admin-sidebar" */).then(c => c.default || c)
+export const LazyDietician_Header = import('../..\\components\\dietician_includes\\Dietician_Header.vue' /* webpackChunkName: "components/dietician-header" */).then(c => c.default || c)
+export const LazyDietician_Sidebar = import('../..\\components\\dietician_includes\\Dietician_Sidebar.vue' /* webpackChunkName: "components/dietician-sidebar" */).then(c => c.default || c)
